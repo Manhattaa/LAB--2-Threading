cb0da2e5d93d69e2914d24b6407f6632adb034c7
Fady <fadynhatta@gmail.com>
2023-12-15 11:42:39 +0100

    Added sound to my project

--- LAB--2 Threading/Program.cs
+++ LAB--2 Threading/Program.cs
@@ -1,136 +1,155 @@
-﻿namespace LAB__2_Threading
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Threading;
-
-    class Car
+﻿    namespace LAB__2_Threading
     {
-        public string Name { get; }
-        public int Speed { get; }
-        public double Distance { get; private set; }
-        public bool Running { get; private set; }
+        using System;
+        using System.Collections.Generic;
+        using System.Threading;
+   
 
-        public Car(string name, int speed = 120)
+        class Car
         {
-            Name = name;
-            Speed = speed;
-            Distance = 0;
-            Running = true;
-        }
+            public string Name { get; }
+            public int Speed { get; }
+            public double Distance { get; private set; }
+            public bool Running { get; private set; }
 
-        public void SimulateRace()
-        {
-            while (Running && Distance < 10)
+            public Car(string name, int speed = 120)
             {
-                Thread.Sleep(100);  // Simulating time passing
-                Distance += Speed / 3600.0;  // Converting speed to km/s
+                Name = name;
+                Speed = speed;
+                Distance = 0;
+                Running = true;
+            }
 
-                if (new Random().NextDouble() < 1.0 / 30)
+            public void SimulateRace()
+            {
+                while (Running && Distance < 10)
                 {
-                    HandleEvent();
-                }
-            }
-        }
+                    Thread.Sleep(100);  // Simulating time passing
+                    Distance += Speed / 3600.0;  // Converting speed to km/s
 
-        private void HandleEvent()
-        {
-            var events = new List<(string, double, int)>
-        {
-            ("Slut på bensin", 1.0 / 50, 30000),
-            ("Punktering", 2.0 / 50, 20000),
-            ("Fågel på vindrutan", 5.0 / 50, 10000),
-            ("Motorfel", 10.0 / 50, -1),
-            ("Turbo-boost", 1.0 / 50, 15000),
-            ("Plötslig regnstorm", 3.0 / 50, 20000)
-        };
-
-            var random = new Random();
-            var eventIndex = random.Next(events.Count);
-            var (eventName, _, duration) = events[eventIndex];
-
-            Console.WriteLine($"{Name} har fått ett problem: {eventName}");
-            Thread.Sleep(duration);
-            Console.WriteLine($"{Name} fortsätter tävla!");
-        }
-    }
-
-    class Program
-    {
-        static void PrintRaceStatus(List<Car> cars)
-        {
-            Console.WriteLine("\nTävlingsstatus:");
-            foreach (var car in cars)
-            {
-                Console.WriteLine($"{car.Name}: {car.Distance:F2} km, Hastighet: {car.Speed} km/h");
-            }
-            Console.WriteLine();
-        }
-
-        static void Main()
-        {
-            var car1 = new Car("Bil 1");
-            var car2 = new Car("Bil 2");
-            var car3 = new Car("Bil 3");
-            var car4 = new Car("Bil 4");
-            var car5 = new Car("Bil 5");
-
-
-            var cars = new List<Car> { car1, car2, car3, car4, car5 };
-
-            var threads = new List<Thread>();
-            foreach (var car in cars)
-            {
-                var thread = new Thread(car.SimulateRace);
-                threads.Add(thread);
-            }
-
-            Console.WriteLine("Tävlingen har börjat!");
-            foreach (var thread in threads)
-            {
-                thread.Start();
-            }
-
-            while (cars.Exists(car => car.Running))
-            {
-                Thread.Sleep(1000);  // Check race status every 1 second
-                PrintRaceStatus(cars);
-            }
-
-            var winner = cars.FindMax(car => car.Distance);
-            Console.WriteLine($"\n{winner.Name} vann tävlingen!");
-        }
-    }
-
-    public static class Extensions
-    {
-        public static T FindMax<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
-            where TKey : IComparable<TKey>
-        {
-            using (var enumerator = source.GetEnumerator())
-            {
-                if (!enumerator.MoveNext())
-                {
-                    throw new InvalidOperationException("Sequence contains no elements");
-                }
-
-                var maxElement = enumerator.Current;
-                var maxValue = selector(maxElement);
-
-                while (enumerator.MoveNext())
-                {
-                    var element = enumerator.Current;
-                    var value = selector(element);
-
-                    if (value.CompareTo(maxValue) > 0)
+                    if (new Random().NextDouble() < 1.0 / 30)
                     {
-                        maxElement = element;
-                        maxValue = value;
+                        HandleEvent();
                     }
                 }
+            }
 
-                return maxElement;
+            private void HandleEvent()
+            {
+                var events = new List<(string, double, int)>
+            {
+                ("Slut på bensin", 1.0 / 50, 30000),
+                ("Punktering", 2.0 / 50, 20000),
+                ("Fågel på vindrutan", 5.0 / 50, 10000),
+                ("Krash", 1.0 / 1000, -1),
+                ("Turbo-boost", 1.0 / 50, 15000),
+                ("Plötslig regnstorm", 3.0 / 50, 20000),
+                ("Motorfel", 10.0 / 50, 20000),
+                ("Kollision", 1.0 / 500, -1),
+            };
+
+                var random = new Random();
+                var eventIndex = random.Next(events.Count);
+                var (eventName, _, duration) = events[eventIndex];
+
+                Console.WriteLine($"{Name} har fått ett problem: {eventName}");
+                Thread.Sleep(duration);
+                Console.WriteLine($"{Name} fortsätter tävla!");
+            }
+        }
+
+
+
+        class Program
+        {
+        static void PlayBackgroundMusic()
+        {
+            // Specify the filename of your background music
+            string backgroundMusicFileName = "tokyo.wav";
+
+            // Play the background music
+            Sounds.PlaySound(backgroundMusicFileName);
+        }
+        static void PrintRaceStatus(List<Car> cars)
+            {
+                Console.WriteLine("\nTävlingsstatus:");
+                foreach (var car in cars)
+                {
+                    Console.WriteLine($"{car.Name}: {car.Distance:F2} km, Hastighet: {car.Speed} km/h");
+                }
+                Console.WriteLine();
+            }
+
+            static void Main()
+            {
+                var car1 = new Car("Bil 1");
+                var car2 = new Car("Bil 2");
+                var car3 = new Car("Bil 3");
+                var car4 = new Car("Bil 4");
+                var car5 = new Car("Bil 5");
+
+                var cars = new List<Car> { car1, car2, car3, car4, car5 };
+
+                var threads = new List<Thread>();
+                foreach (var car in cars)
+                {
+                    var thread = new Thread(car.SimulateRace);
+                    threads.Add(thread);
+                }
+
+                Console.WriteLine("Tävlingen har börjat!");
+                foreach (var thread in threads)
+                {
+                    thread.Start();
+                }
+            PlayBackgroundMusic();
+
+            while (cars.Exists(car => car.Running))
+                {
+                    Thread.Sleep(1000);  // Check race status every 1 second
+                    PrintRaceStatus(cars);
+                }
+
+                var winner = cars.FindMax(car => car.Distance);
+                Console.WriteLine($"\n{winner.Name} vann tävlingen!");
+
+                // Ensure all threads are finished before exiting
+                foreach (var thread in threads)
+                {
+                    thread.Join();
+                }
+            }
+        }
+
+        public static class Extensions
+        {
+            public static T FindMax<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
+                where TKey : IComparable<TKey>
+            {
+                using (var enumerator = source.GetEnumerator())
+                {
+                    if (!enumerator.MoveNext())
+                    {
+                        throw new InvalidOperationException("Sequence contains no elements");
+                    }
+
+                    var maxElement = enumerator.Current;
+                    var maxValue = selector(maxElement);
+
+                    while (enumerator.MoveNext())
+                    {
+                        var element = enumerator.Current;
+                        var value = selector(element);
+
+                        if (value.CompareTo(maxValue) > 0)
+                        {
+                            maxElement = element;
+                            maxValue = value;
+                        }
+                    }
+
+                    return maxElement;
+                }
             }
         }
     }
-}
---
+++ LAB--2 Threading/Sounds.cs
@@ -0,0 +1,36 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB__2_Threading
+{
+    internal class Sounds
+    {
+        private static WaveOutEvent waveOut = new WaveOutEvent();
+
+        public static void PlaySound(string soundFileName)
+        {
+            string soundFilePath = Path.Combine("Sounds", soundFileName);
+
+            try
+            {
+                using (var audioFile = new AudioFileReader(soundFilePath))
+                {
+                    waveOut.Init(audioFile);
+                    waveOut.Play();
+                    while (waveOut.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing sound: {ex.Message}");
+            }
+        }
+    }
+}
